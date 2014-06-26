58fe528f39dff5b31ecc3d1d33f1d268b5dadbe7
tien <truong.htien@gmail.com>
2014-06-26 13:31:48 -0400

    0.0.4 - Index Banner

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: "CityVor"});
+  res.render('index', { title: "Cityvore"});
 });
 
 module.exports = router;
